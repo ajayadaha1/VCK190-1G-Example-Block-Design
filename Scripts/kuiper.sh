2df4fa2a8d1d909abdff860123d9c0f264721005
Ajaya Dahal <ajayad@amd.com>
2025-05-13 11:54:30 -0700

    Add Amazon patch script

---
+++ Scripts/kuiper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -f
+
+#export XILINX_PATH=$XILINX_PATH:/group/bcapps/coryb/patches_2022.1/patch/AR34269/vivado:/group/bcapps/coryb/patches_2022.1/AR34269/vivado:/group/bcapps/coryb/patches_2022.1/AR34317/vivado:/group/bcapps/coryb/patches_2022.1/AR34465/vivado:/group/bcapps/coryb/patches_2022.1/AR000034624/vivado:/group/bcapps/coryb/patches_2022.1/AR000035115/vivado
+
+#export XILINX_PATH=$XILINX_PATH:/group/bcapps/coryb/patches_2022.1/patch/AR34269/vivado:/group/bcapps/coryb/patches_2022.1/AR34269/vivado:/group/bcapps/coryb/patches_2022.1/AR34317/vivado:/group/bcapps/coryb/patches_2022.1/AR000034624/vivado:/group/bcapps/coryb/patches_2022.1/AR000035115/vivado:/group/bcapps/coryb/patches_2022.1/patch/AR36040/vivado:/group/bcapps/coryb/patches_2022.1/AR36040/vivado
+
+#export XILINX_PATH=$XILINX_PATH:/group/bcapps/coryb/patches_2022.1/patch/AR34269/vivado:/group/bcapps/coryb/patches_2022.1/AR34269/vivado:/group/bcapps/coryb/patches_2022.1/AR34317/vivado:/group/bcapps/coryb/patches_2022.1/AR000034624/vivado:/group/bcapps/coryb/patches_2022.1/AR000035115/vivado:/group/bcapps/coryb/patches_2022.1/patch/AR36040/vivado:/group/bcapps/coryb/patches_2022.1/AR36040/vivado:/group/bcapps/coryb/patches_2022.1/AR34465/vivado
+
+export XILINX_PATH=$XILINX_PATH:/group/bcapps/coryb/patches_2022.1/patch/AR34269/vivado:/group/bcapps/coryb/patches_2022.1/AR34269/vivado:/group/bcapps/coryb/patches_2022.1/AR34317/vivado:/group/bcapps/coryb/patches_2022.1/AR000034624/vivado:/group/bcapps/coryb/patches_2022.1/AR34723/vivado:/group/bcapps/coryb/patches_2022.1/AR000035115/vivado:/group/bcapps/coryb/patches_2022.1/patch/AR36040/vivado:/group/bcapps/coryb/patches_2022.1/AR36040/vivado
+
+#:/group/bcapps/coryb/patches_2022.1/AR35281/vivado
